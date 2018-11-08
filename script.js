327a2c5b3dcaeec56a22c19e1e27ebf49052256e
Thomas Pichard <pichard.thomas@icloud.com>
2018-11-08 14:48:40 +0100

    get commit works

--- script.js
+++ script.js
@@ -4,50 +4,38 @@ var repoAddress = "https://api.github.com/repos/";
 var userAddress = "https://api.github.com/users/";
 var repoName = "github-ynov-vue";
 
+var client_id ="74d5bc2e581ebb60dcc7";
+var client_secret ="45de24cc6d2e2dfa6bdff1fc59a0af22e4409b4a";
+
 
 var vue = new Vue({
     el: '#app' ,
     methods: {
 
-        getCommits: function(){
-            for (user in githubNames){
+        getCommits: function () {
 
+            for (user in this.userSelected) {
+
+                var newAccount = {};
+                newAccount.name = githubNames[user];
+                newAccount.commit = [];
+
+                console.log(user);
                 axios
-                    .get(repoAddress+this.userSelected[user]+'/'+repoName+'/commits')
-                    .then(function (response){
-                        for (result in response){
+                    .get(repoAddress + githubNames[user] + '/' + repoName + '/commits?client_id=' + client_id + '&client_secret=' + client_secret)
+                    .then(function (response) {
 
-                         }
-                    });
-            }
-        },
+                        console.log(response);
 
-        getGithubAccounts: function(){
-            var users = [];
-
-            var newAccount = {};
-
-            for (user in this.userSelected){
-
-                newAccount.name = this.userSelected[user];
-
-                axios
-                    .get(repoAddress+this.userSelected[user]+'/'+repoName+'/commits')
-                    .then(function (response){
-
-                        for (result in response){
-                            //users.commit.append(result);
+                        for (result in response) {
+                            newAccount.commit += result;
                         }
 
                     });
-
-                users += newAccount;
+                this.githubAccounts += newAccount
             }
-            this.githubAccounts = users;
-            console.log(this.githubAccounts)
-        }
 
-
+        },
     },
     data: {
         githubAccounts: [],
@@ -57,9 +45,7 @@ var vue = new Vue({
         userSelected: githubNames,
 
     }, created(){
-
-        this.getGithubAccounts();
-
+        this.getCommits();
     }
-
 });
+
