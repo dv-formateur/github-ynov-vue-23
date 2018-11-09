bd184ee8dd9db79673c413867c38a002c6b64a26
Thomas Pichard <pichard.thomas@icloud.com>
2018-11-09 11:29:31 +0100

    Commits are showing on main page

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 var githubNames = ["Killy85",  "Nair0fl",  "raphaelCharre", "mathiasLoiret", "thomaspich", "TeofiloJ", "Grigusky", "Dakistos", "mael61", "KevinPautonnier", "BenoitCochet", "sfongue", "ClementCaillaud", "gfourny", "Mokui", "LordInateur", "AntoineGOSSET", "etienneYnov", "Coblestone", "AlexDesvallees", "rudy8530", "benjaminbra", "mael61", "alixnzt"];
 
+
 var repoAddress = "https://api.github.com/repos/";
-var userAddress = "https://api.github.com/users/";
-var repoName = "github-ynov-vue";
+var repos = ["github-ynov-vue", "gta-ynov-vue"];
 
-var client_id ="74d5bc2e581ebb60dcc7";
-var client_secret ="45de24cc6d2e2dfa6bdff1fc59a0af22e4409b4a";
+var client_id ="0beaa261583901a5aa83";
+var client_secret ="baff1c994b9a1c21432bcb0e5cd7635ebe445ed1";
 
+var vm;
+
+// Choix projet / Choix période / choix users
 
 
 var vue = new Vue({
     el: '#app',
     data: {
         names: githubNames,
+        usersSelected: [],
+        startDate: "",
+        endDate: "",
+        repos: repos,
+        repoSelected: repos[0]
+
+    }, created(){
+
+        this.getSelectedUsers();
 
     },
 
     methods: {
-        getCommits: function(user){
 
-            var value;
+        getSelectedUsers: function() {
 
-            axios
-                .get(repoAddress + user + '/' + repoName + '/commits?client_id=' + client_id + '&client_secret=' + client_secret)
-                .then(function (response) {
-                    //console.log(response.data);
-                    value = response.data;
-                    console.log(value)
-                });
+            this.names.forEach(this.getSelectedUser)
+        },
 
-            return "hello";
+
+
+        getSelectedUser: function(user) {
+
+            vm = this;
+                axios
+                    .get(repoAddress +user + '/' + vm.repoSelected + '/commits?client_id=' + client_id + '&client_secret=' + client_secret)
+                    .then(function (response) {
+
+                        //console.log(nom)
+
+                        vm.usersSelected.push({
+                            name: user,
+                            commit: response.data.concat()
+                        });
+
+
+                    });
 
         }
-    }, created(){
-
     }
-    
 });
